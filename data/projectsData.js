1f0f9cc303a1bfa1234c6302478b83f3af512792
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-04-24 17:10:48 +0200

    edit projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,14 +5,6 @@ const projectsData = [
     imgSrc: '/static/images/google.png',
     href: 'https://play.google.com/store/apps/details?id=com.naturestudios.benchmark&hl=de&gl=US',
   },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
 ]
 
 export default projectsData
